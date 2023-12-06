924d313c67f017a8ee86bda9f14f2a4d40e27145
min <alsruddl1119@naver.com>
2023-12-07 00:06:09 +0900

    ObjectPool<T> 추가
    - ObjectPool<T> 클래스 추가

---
+++ Devoram/DevoramUtility/ObjectPool.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Concurrent;
+using System.Diagnostics.CodeAnalysis;
+
+namespace DevoramUtility
+{
+    public interface IRecycle
+    {
+        public abstract void Reset();
+        public abstract void Init(params object?[]? args);
+    }
+
+    public class ObjectPool<T> where T : IRecycle, new()
+    {
+        private readonly int DefaultCapacity = 256;
+
+        private ConcurrentList<T> _objects = new();
+
+        private ConcurrentQueue<T> _waitingObjectQueue = new();
+
+        public ObjectPool(int defaultCapacity = 0)
+        {
+            if (defaultCapacity > 0)
+            {
+                DefaultCapacity = defaultCapacity;
+            }
+
+            for (int i = 0; i < DefaultCapacity; i++)
+            {
+                CreateObject();
+            }
+        }
+
+        [return: NotNull]
+        private T CreateObject(bool enqueue = true)
+        {
+            T obj = new T();
+            _objects.Add(obj);
+
+            if (enqueue)
+            {
+                _waitingObjectQueue.Enqueue(obj);
+            }
+
+            return obj;
+        }
+
+
+        [return: NotNull]
+        public T GetObject()
+        {
+            if (false == _waitingObjectQueue.TryDequeue(out T? resultObject))
+            {
+                return CreateObject(false);
+            }
+
+            return resultObject;
+        }
+
+        public void ReturnObject([NotNull]T obj)
+        {
+            if(false == _objects.Contains(obj))
+            {
+                return;
+            }
+
+            obj.Reset();
+            _waitingObjectQueue.Enqueue(obj);
+        }
+    }
+}
