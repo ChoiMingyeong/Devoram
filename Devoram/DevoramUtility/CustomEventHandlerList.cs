3b097cd16d03be1e435fdeaea0a205c1b4e8a8be
min <alsruddl1119@naver.com>
2023-12-12 00:36:15 +0900

    CustomEventHandler 제작
    - CustomeEventHandlerList 클래스 추가
    - CustomEventHandlerManger 클래스 추가

---
+++ Devoram/DevoramUtility/CustomEventHandlerList.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Concurrent;
+using System.ComponentModel;
+
+namespace DevoramUtility
+{
+    public class CustomEventHandlerList
+    {
+        private static ObjectPool<EventKey> _eventKeyObjectPool = new ObjectPool<EventKey>();
+
+        private ConcurrentDictionary<byte, EventKey> _eventKeyMapping = new();
+        private EventHandlerList _eventHandlerList = new EventHandlerList();
+
+        private EventHandler? this[byte index]
+        {
+            get
+            {
+                if (false == _eventKeyMapping.TryGetValue(index, out var key))
+                {
+                    return null;
+                }
+
+                if (_eventHandlerList[key] is not EventHandler eventHandler)
+                {
+                    return null;
+                }
+
+                return eventHandler;
+            }
+        }
+
+        public void AddHandler(byte index, EventHandler? eventHandler)
+        {
+            if (false == _eventKeyMapping.TryGetValue(index, out var key))
+            {
+                key = _eventKeyObjectPool.GetObject();
+                _eventKeyMapping.TryAdd(index, key);
+            }
+
+            _eventHandlerList.AddHandler(key, eventHandler);
+        }
+
+        public void RemoveHandler(byte index, EventHandler? eventHandler)
+        {
+            if (false == _eventKeyMapping.TryGetValue(index, out var key))
+            {
+                return;
+            }
+
+            _eventKeyObjectPool.ReturnObject(key);
+            _eventHandlerList.RemoveHandler(key, eventHandler);
+        }
+
+        public void NotifyRaisedEvent(byte index, EventArgs e)
+        {
+            if (false == _eventKeyMapping.TryGetValue(index, out var key))
+            {
+                return;
+            }
+
+            this[index]?.Invoke(this, e);
+        }
+    }
+}
---
+++ Devoram/DevoramUtility/CustomEventHandlerManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Concurrent;
+
+namespace DevoramUtility
+{
+    public class CustomEventHandlerManager<T> where T : notnull
+    {
+        private ConcurrentDictionary<T, CustomEventHandlerList> _targetEventHandlerMapping = new();
+
+        public void AddEventDelegate(T target, byte index, EventHandler? eventHandler)
+        {
+            if (false == _targetEventHandlerMapping.TryGetValue(target, out var handlers))
+            {
+                handlers = new CustomEventHandlerList();
+                _targetEventHandlerMapping.TryAdd(target, handlers);
+            }
+
+            handlers.AddHandler(index, eventHandler);
+        }
+
+        public void RemoveEventDelegate(T target, byte index, EventHandler? eventHandler)
+        {
+            if (false == _targetEventHandlerMapping.TryGetValue(target, out var handlers))
+            {
+                return;
+            }
+
+            handlers.RemoveHandler(index, eventHandler);
+        }
+
+        public void NotifyRaisedEvent(T target, byte index, EventArgs e)
+        {
+            if (false == _targetEventHandlerMapping.TryGetValue(target, out var handlers))
+            {
+                return;
+            }
+
+            handlers.NotifyRaisedEvent(index, e);
+        }
+    }
+}
